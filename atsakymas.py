719ee1de9453c82a00b3c3ccfcb59459f5b5d455
Andrius8777 <andrius001@protonmail.com>
2024-01-12 08:10:19 +0200

    dirbam

---
+++ atsakymas.py
@@ -0,0 +1,7 @@
+from geometrija import apskritimas as apskr
+print(f'{apskr.apskritimo_plotas(10):.2f}')
+print(apskr.kvadratas.kvadrato_plotas(4))
+
+from geometrija.geometrija2.kvadratas import kvadrato_plotas as plotas
+print(plotas(2))
+
---
+++ geometrija/apskritimas.py
@@ -0,0 +1,5 @@
+def apskritimo_plotas(r):
+    from math import pi 
+    return pi * (r ** 2)
+
+from .geometrija2 import kvadratas
